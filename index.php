507bd4578e7bbadaa18f36905d3556f9a6f1cc44
Giannicola Speranza <speranzagiannicola@gmail.com>
2025-04-17 15:27:35 +0200

    sesto esercizio

--- index.php
+++ index.php
@@ -26,18 +26,35 @@
 
 // secondo esercizio
 
-$numbers = [10, 7, 6, 4, 12, 9, 3];
-$count = 0;
-$sum = 0;
+// $numbers = [10, 7, 6, 4, 12, 9, 3];
+// $count = 0;
+// $sum = 0;
 
-foreach ($numbers as $number){
-    if(($number % 2) === 0){
-        $sum = $sum + $number;
-        $count++;
+// foreach ($numbers as $number){
+//     if(($number % 2) === 0){
+//         $sum = $sum + $number;
+//         $count++;
 
-    }
+//     }
 
-}
-echo $sum / $count; 
+// }
+// echo $sum / $count; 
 
 // fine secondo esercizio
+
+
+// inizio terzo esercizio
+
+for ($i=1; $i <= 100 ; $i++) { 
+    if($i % 3 == 0 && $i % 5 == 0){
+        echo "HACKADEMY\n";
+    }elseif ($i % 5 == 0) {
+        echo "JAVASCRIPT\n";
+    }elseif ($i % 3 == 0) {
+        echo "PHP\n";
+    }else{
+        echo $i ."\n";
+    }
+}
+
+// fine terzo esercizio 
