2b09917062c4b39da85cf79f95a55ef97256f479
Giannicola Speranza <speranzagiannicola@gmail.com>
2025-04-17 15:19:04 +0200

    quinto esercizio

--- index.php
+++ index.php
@@ -1,22 +1,43 @@
 <?php
 
 
-$users = [
-    ['name' => 'Davide', 'surname' => 'Cariola', 'gender' => 'NB'],
-    ['name' => 'Luca', 'surname' => 'Gino', 'gender' => 'M'],
-    ['name' => 'Laura', 'surname' => 'Laurenti', 'gender' => 'F'],
-    ['name' => 'Gianni', 'surname' => 'Calenda', 'gender' => 'M'],
- ];
+// $users = [
+//     ['name' => 'Davide', 'surname' => 'Cariola', 'gender' => 'NB'],
+//     ['name' => 'Luca', 'surname' => 'Gino', 'gender' => 'M'],
+//     ['name' => 'Laura', 'surname' => 'Laurenti', 'gender' => 'F'],
+//     ['name' => 'Gianni', 'surname' => 'Calenda', 'gender' => 'M'],
+//  ];
 
- foreach ($users as $user){
-    if($user['gender'] == 'F'){
-        echo 'Buongiorno Sig.ra' . " " . $user['name'] . " " . $user['surname'] . " \n" ;
-    }elseif($user['gender'] == 'M'){
-        echo 'Buongiorno Sig.' . " " . $user['name'] . " " . $user['surname'] . " \n"  ;
+//  foreach ($users as $user){
+//     if($user['gender'] == 'F'){
+//         echo 'Buongiorno Sig.ra' . " " . $user['name'] . " " . $user['surname'] . " \n" ;
+//     }elseif($user['gender'] == 'M'){
+//         echo 'Buongiorno Sig.' . " " . $user['name'] . " " . $user['surname'] . " \n"  ;
 
-    }else{
-        echo 'Buongiorno' . " " . $user['name'] . " " . $user['surname']. " \n"  ;
+//     }else{
+//         echo 'Buongiorno' . " " . $user['name'] . " " . $user['surname']. " \n"  ;
         
-    }
-}
+//     }
+// }
 
+// fine primo esercizio
+
+
+
+// secondo esercizio
+
+$numbers = [10, 7, 6, 4, 12, 9, 3];
+$count = 0;
+$sum = 0;
+
+foreach ($numbers as $number){
+    if(($number % 2) === 0){
+        $sum = $sum + $number;
+        $count++;
+
+    }
+
+}
+echo $sum / $count; 
+
+// fine secondo esercizio
