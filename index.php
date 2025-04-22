1e056a62dacba2c7f2808f7b03ca5ec8c2a4cd47
Giannicola Speranza <speranzagiannicola@gmail.com>
2025-04-22 17:33:53 +0200

    selfwork php oop 2 completato

--- index.php
+++ index.php
@@ -45,16 +45,96 @@
 
 // inizio terzo esercizio
 
-for ($i=1; $i <= 100 ; $i++) { 
-    if($i % 3 == 0 && $i % 5 == 0){
-        echo "HACKADEMY\n";
-    }elseif ($i % 5 == 0) {
-        echo "JAVASCRIPT\n";
-    }elseif ($i % 3 == 0) {
-        echo "PHP\n";
-    }else{
-        echo $i ."\n";
+// for ($i=1; $i <= 100 ; $i++) { 
+//     if($i % 3 == 0 && $i % 5 == 0){
+//         echo "HACKADEMY\n";
+//     }elseif ($i % 5 == 0) {
+//         echo "JAVASCRIPT\n";
+//     }elseif ($i % 3 == 0) {
+//         echo "PHP\n";
+//     }else{
+//         echo $i ."\n";
+//     }
+// }
+
+// fine terzo esercizio 
+// inizio quarto esercizio
+
+
+
+// Utilizza i principi di OOP ed Ereditarieta' per creare una struttura a classi come la seguente:
+// +-|Continent
+// +-----------|Country
+// +--------------------|Region
+// +---------------------------|Province
+// +------------------------------------|City
+// +------------------------------------------|Street
+
+class Continent
+{
+  public $nameContinent;
+  
+  public function __construct($continent)
+  {
+    $this->nameContinent = $continent;
+  }
+  
+}
+
+class Country extends Continent{
+    public $nameCountry;
+
+    public function __construct($continent, $country){
+    parent::__construct($continent);
+    $this->nameCountry = $country;
+}
+}
+
+class Region extends Country{
+
+    public $nameRegion;
+
+    public function __construct($continent, $country,$region){
+        parent::__construct($continent, $country);
+        $this->nameRegion = $region;
     }
 }
 
-// fine terzo esercizio 
+class Province extends Region{
+    public $nameProvince;
+
+    public function __construct($continent, $country,$region, $province){
+    parent:: __construct($continent, $country, $region);
+    $this->nameProvince = $province;
+}
+}
+
+
+class City extends Province{
+    public $nameCity;
+
+    public function __construct($continent, $country,$region, $province,$city){
+    parent:: __construct($continent, $country, $region,$province); 
+    $this->nameCity = $city;
+
+    }
+}
+
+
+class Street extends City{
+    public $nameStreet;
+
+    public function __construct($continent, $country,$region, $province,$city,$street){
+        parent:: __construct($continent, $country, $region,$province,$city); 
+        $this->nameStreet  =$street;
+
+    }
+    
+    public function getMyCurrentLocation(){
+        echo "Mi trovo in " . $this->nameContinent . ", " . $this->nameCountry . ", " . $this->nameRegion . ", " .$this->nameProvince . ", " . $this->nameCity . ", " . $this->nameStreet . ".";
+    }
+}
+
+$myLocation = new Street ("Europa", "Italia", "Puglia","BA","Bari","Strada San Giorgio Martire 2D");
+
+$myLocation->getMyCurrentLocation();
