2b3068172b4770932cd667038fa0ae2c2c01c2bf
Giannicola Speranza <speranzagiannicola@gmail.com>
2025-04-23 16:12:09 +0200

    selfwork php oop 3 completato

--- index.php
+++ index.php
@@ -70,71 +70,172 @@
 // +------------------------------------|City
 // +------------------------------------------|Street
 
-class Continent
-{
-  public $nameContinent;
+// class Continent
+// {
+//   public $nameContinent;
   
-  public function __construct($continent)
-  {
-    $this->nameContinent = $continent;
-  }
+//   public function __construct($continent)
+//   {
+//     $this->nameContinent = $continent;
+//   }
   
-}
+// }
 
-class Country extends Continent{
-    public $nameCountry;
+// class Country extends Continent{
+//     public $nameCountry;
 
-    public function __construct($continent, $country){
-    parent::__construct($continent);
-    $this->nameCountry = $country;
-}
-}
+//     public function __construct($continent, $country){
+//     parent::__construct($continent);
+//     $this->nameCountry = $country;
+// }
+// }
 
-class Region extends Country{
+// class Region extends Country{
 
-    public $nameRegion;
+//     public $nameRegion;
 
-    public function __construct($continent, $country,$region){
-        parent::__construct($continent, $country);
-        $this->nameRegion = $region;
+//     public function __construct($continent, $country,$region){
+//         parent::__construct($continent, $country);
+//         $this->nameRegion = $region;
+//     }
+// }
+
+// class Province extends Region{
+//     public $nameProvince;
+
+//     public function __construct($continent, $country,$region, $province){
+//     parent:: __construct($continent, $country, $region);
+//     $this->nameProvince = $province;
+// }
+// }
+
+
+// class City extends Province{
+//     public $nameCity;
+
+//     public function __construct($continent, $country,$region, $province,$city){
+//     parent:: __construct($continent, $country, $region,$province); 
+//     $this->nameCity = $city;
+
+//     }
+// }
+
+
+// class Street extends City{
+//     public $nameStreet;
+
+//     public function __construct($continent, $country,$region, $province,$city,$street){
+//         parent:: __construct($continent, $country, $region,$province,$city); 
+//         $this->nameStreet  =$street;
+
+//     }
+    
+//     public function getMyCurrentLocation(){
+//         echo "Mi trovo in " . $this->nameContinent . ", " . $this->nameCountry . ", " . $this->nameRegion . ", " .$this->nameProvince . ", " . $this->nameCity . ", " . $this->nameStreet . ".";
+//     }
+// }
+
+// $myLocation = new Street ("Europa", "Italia", "Puglia","BA","Bari","Strada San Giorgio Martire 2D");
+
+// $myLocation->getMyCurrentLocation();
+
+// fine quarto esercizio 
+
+
+// inizio quinto esercizio
+
+class Vertebrati{
+    
+    public function __construct(){
+        $this->stampaVertebrati();
+    }
+
+    protected function stampaVertebrati(){
+        echo "Sono un animale Vertebrato  \n";
     }
 }
 
-class Province extends Region{
-    public $nameProvince;
+class SangueCaldo extends Vertebrati{
 
-    public function __construct($continent, $country,$region, $province){
-    parent:: __construct($continent, $country, $region);
-    $this->nameProvince = $province;
-}
-}
+    public function __construct(){
+        parent::__construct();
+        $this->stampaSangueCaldo();
+    }
 
-
-class City extends Province{
-    public $nameCity;
-
-    public function __construct($continent, $country,$region, $province,$city){
-    parent:: __construct($continent, $country, $region,$province); 
-    $this->nameCity = $city;
-
+    protected function stampaSangueCaldo(){
+        echo "Sono un animale a Sangue caldo \n";
     }
 }
 
+class Mammiferi extends SangueCaldo{
 
-class Street extends City{
-    public $nameStreet;
+    public function __construct(){
+        parent::__construct();
+        $this->stampaMammiferi();
+    }
 
-    public function __construct($continent, $country,$region, $province,$city,$street){
-        parent:: __construct($continent, $country, $region,$province,$city); 
-        $this->nameStreet  =$street;
+protected function stampaMammiferi(){
+        echo "Sono un Mammifero \n";
+    }
+}
 
+class Uccelli extends SangueCaldo{
+    
+    public function __construct(){
+        parent::__construct();
+        $this->stampaUccelli();
     }
     
-    public function getMyCurrentLocation(){
-        echo "Mi trovo in " . $this->nameContinent . ", " . $this->nameCountry . ", " . $this->nameRegion . ", " .$this->nameProvince . ", " . $this->nameCity . ", " . $this->nameStreet . ".";
+    protected function stampaUccelli(){
+        echo "Sono un Uccello \n";
     }
 }
 
-$myLocation = new Street ("Europa", "Italia", "Puglia","BA","Bari","Strada San Giorgio Martire 2D");
 
-$myLocation->getMyCurrentLocation();
+
+
+class SangueFreddo extends Vertebrati{
+    public function __construct(){
+        parent::__construct();
+        $this->stampaSangueFreddo();
+    }
+    
+    protected function stampaSangueFreddo(){
+        echo "Sono un animale a Sangue Freddo \n";
+    }
+}
+
+class Pesci extends SangueFreddo{
+    public function __construct(){
+        parent::__construct();
+        $this->stampaPesci();
+    }
+    
+    protected function stampaPesci(){
+        echo "Sono un Pesce \n";
+    }
+}
+
+class Rettili extends SangueFreddo{
+    public function __construct(){
+        parent::__construct();
+        $this->stampaRettili();
+    }
+    
+    protected function stampaRettili(){
+        echo "Sono un Rettile \n";
+    }
+}
+
+class Anfibi extends SangueFreddo{
+    public function __construct(){
+        parent::__construct();
+        $this->stampaAnfibi();
+    }
+    
+    protected function stampaAnfibi(){
+        echo "Sono un Amfibio \n";
+    }
+}
+
+$piccione = new Uccelli();
